59bfb40bf1a71aaab6bdb609615d68273604a9d8
Harishraj C <harishcc2007@gmail.com>
2026-07-23 21:12:51 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,14 @@ app.get("/",(req,res)=>{
 })
 
 mongoose.connect(MONGO_URI)
-    .then(() => console.log("Database connected successfully"))
+    .then(() => {
+        console.log("Database connected successfully");
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
     .catch((err) => {
-        console.error("CRITICAL ERROR:", err.message); 
+        console.error("CRITICAL ERROR:", err.message);
+        process.exit(1);
     });
